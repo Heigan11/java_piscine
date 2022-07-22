215c873754d984c9df8fc2327a8c821841ebff1e
Heigan <heigan11@gmail.com>
2022-07-22 17:37:22 +0300

    day05 ex00 в работе

---
+++ day05/ex00/Chat/src/main/java/edu/school21/chat/models/ChatRoom.java
@@ -0,0 +1,11 @@
+package edu.school21.chat.models;
+
+import java.util.List;
+
+public class ChatRoom {
+    private int id;
+    private String name;
+    private ChatUser owner;
+    private List<ChatMessage> chatMessageList;
+
+}
