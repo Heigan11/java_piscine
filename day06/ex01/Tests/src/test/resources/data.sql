c7515c75c2a53f6f56ec20a6aae14e9e6912cb79
Heigan <heigan11@gmail.com>
2022-07-27 12:37:34 +0300

    day06 ex01 added

---
+++ day06/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (name, price) VALUES ('Burn', 80);
+INSERT INTO products (name, price) VALUES ('Adrenalin', 100);
+INSERT INTO products (name, price) VALUES ('RedBull', 130);
+INSERT INTO products (name, price) VALUES ('Slurm', 100500);
+INSERT INTO products (name, price) VALUES ('Monster', 15);
---
+++ day06/ex01/Tests/src/test/resources/schema.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE if not exist products (
+    identifier IDENTITY PRIMARY KEY,
+    name VARCHAR(30),
+    price INTEGER
+);
