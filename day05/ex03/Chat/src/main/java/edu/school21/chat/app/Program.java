54a45ddb3a798a2474c22a7d7ab181c22407fb85
Heigan <heigan11@gmail.com>
2022-07-27 12:00:33 +0300

    day06 ex00 added

--- day05/ex03/Chat/src/main/java/edu/school21/chat/app/Program.java
+++ day05/ex03/Chat/src/main/java/edu/school21/chat/app/Program.java
@@ -1,14 +1,11 @@
 package edu.school21.chat.app;
 
 import com.zaxxer.hikari.HikariDataSource;
-import edu.school21.chat.models.Chatroom;
 import edu.school21.chat.models.Message;
-import edu.school21.chat.models.User;
 import edu.school21.chat.repositories.MessagesRepository;
 import edu.school21.chat.repositories.MessagesRepositoryJdbcImpl;
 
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class Program {
